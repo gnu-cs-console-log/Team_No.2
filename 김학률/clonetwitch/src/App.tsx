5de2e06c85d2398ae694af6476f20b0f419f326c
markyul <pig052656@naver.com>
2021-07-14 23:24:10 +0900

    header 구현

--- 김학률/clonetwitch/src/App.tsx
+++ 김학률/clonetwitch/src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
 import './App.css';
-import Header from './components/HeaderNav';
+import Header from './components/Header/Header';
 import Main from './components/Main';
 import ChannelList from './components/LeftChannelList';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+
+library.add(fab, fas, far)
 
 function App() {
   return (
--- 김학률/clonetwitch/src/components/HeaderNav.tsx
+++
@@ -1,7 +0,0 @@
-export default function HeaderNav() {
-    return (
-        <nav id="headerNavs">
-            <h1>헤더</h1>
-        </nav>
-    )
-}
